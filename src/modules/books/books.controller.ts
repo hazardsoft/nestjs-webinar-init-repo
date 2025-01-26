1899373c5e1b59c378db02414f280b7d312fa819
Henadzi Shutko <hazardsoft@gmail.com>
2025-01-26 11:37:05 +0300

    Use BooksService to handle requests in BooksController

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -8,24 +8,31 @@ import {
   Put,
 } from '@nestjs/common';
 import { BooksService } from './books.service';
+import { Book } from './books.entity';
 
 @Controller('books')
 export class BooksController {
   constructor(private readonly booksService: BooksService) {}
   @Get()
-  async getAll() {
-    return [];
+  async getAll(): Promise<Book[]> {
+    return this.booksService.getAllBooks();
   }
 
   @Get(':id')
-  async getBookById(@Param('id') id: number) {}
+  async getBookById(@Param('id') id: number): Promise<Book> {
+    return this.booksService.getBookById(id);
+  }
 
   @Post()
-  async createBook(@Body() bookDto: any) {}
+  async createBook(@Body() bookDto: any): Promise<Book> {
+    return this.booksService.createBook(bookDto);
+  }
 
   @Put()
   async updateBook(@Body() bookDto: any) {}
 
   @Delete(':id')
-  async deleteBook(@Param('id') id: number) {}
+  async deleteBook(@Param('id') id: number): Promise<void> {
+    await this.booksService.deleteBookById(id);
+  }
 }
