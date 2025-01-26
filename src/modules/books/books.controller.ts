3bf16f1efd97f19f8caf6e1d814592ffca018e2b
Henadzi Shutko <hazardsoft@gmail.com>
2025-01-26 11:26:06 +0300

    Add BooksController template

--- src/modules/books/books.controller.ts
+++ src/modules/books/books.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { BooksService } from './books.service';
 
 @Controller('books')
-export class BooksController {}
+export class BooksController {
+  constructor(private readonly booksService: BooksService) {}
+  @Get()
+  async getAll() {
+    return [];
+  }
+
+  @Get(':id')
+  async getBookById(@Param('id') id: number) {}
+
+  @Post()
+  async createBook(@Body() bookDto: any) {}
+
+  @Put()
+  async updateBook(@Body() bookDto: any) {}
+
+  @Delete(':id')
+  async deleteBook(@Param('id') id: number) {}
+}
