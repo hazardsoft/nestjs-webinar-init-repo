c8266deac1677b7cd9e702afcc732ec2c440ce98
Henadzi Shutko <hazardsoft@gmail.com>
2025-01-26 11:36:36 +0300

    Implement methods of BooksService

--- src/modules/books/books.service.ts
+++ src/modules/books/books.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { BooksRepository } from './books.repository';
+import { Book } from './books.entity';
 
 @Injectable()
-export class BooksService {}
+export class BooksService {
+  constructor(private readonly booksRepository: BooksRepository) {}
+
+  async getAllBooks(): Promise<Book[]> {
+    return this.booksRepository.findAll();
+  }
+
+  async getBookById(id: number): Promise<Book> {
+    return this.booksRepository.findOneOrNotFoundFail(id);
+  }
+
+  async createBook(bookDto: any): Promise<Book> {
+    const book = new Book();
+    book.title = bookDto.title;
+    book.author = bookDto.author;
+    book.ageRestriction = bookDto.ageRestriction;
+    return this.booksRepository.save(book);
+  }
+
+  async deleteBookById(id: number): Promise<void> {
+    await this.booksRepository.delete(id);
+  }
+}
